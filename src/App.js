97b98857e1a5a63d2d0ae1489391a20a7229cddf
Rumrage <claravillarruel4@gmail.com>
2023-05-12 00:06:52 -0300

    Categorias completo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import { CategoryProvider } from './Context/CategoryContext';
 
+import { Home } from './components/Home';
+import { CategoryIndex } from './components/categories/CategoryIndex';
+import { CategoryCreate } from './components/categories/CategoryCreate';
+import { CategoryEdit } from './components/categories/CategoryEdit';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <CategoryProvider>
+      <div className="bg-slate-200">
+        <div className="max-w-7xl mx-auto min-h-screen">
+        <nav>
+          <ul className='flex'>
+            <li className='m-2 p-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded-md'>
+              <Link to="/">Home</Link>
+            </li>
+            <li className='m-2 p-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded-md'>
+              <Link to="/categories">Categorias</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/categories" element={<CategoryIndex />} />
+          <Route path="/categories/create" element={<CategoryCreate />} />
+          <Route path="/categories/:id/edit" element={<CategoryEdit />} />
+        </Routes>
+        </div>
+      </div> 
+    </CategoryProvider>
   );
 }
 
