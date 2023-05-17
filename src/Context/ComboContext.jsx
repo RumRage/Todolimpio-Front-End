4635cf957c85ccd08ff7f2a234a799ed4dcc6a91
Rumrage <claravillarruel4@gmail.com>
2023-05-17 17:43:05 -0300

    Punto inicial

--- src/Context/ComboContext.jsx
+++ src/Context/ComboContext.jsx
@@ -7,115 +7,191 @@ axios.defaults.baseURL = "http://127.0.0.1:8000/api/v1/";
 const ComboContext = createContext();
 
 export const ComboProvider = ({ children }) => {
-  const initialForm = {
-    name: "",
-    service_id: [],
-    price: "",
-    discount: "",
-    total_price: ""
-  };
-  const [formValues, setFormValues] = useState(initialForm);
-
-  const [combos, setCombos] = useState([]);
-  const [combo, setCombo] = useState([]);
-  const [errors, setErrors] = useState({});
-  const navigate = useNavigate();
-  const [services, setServices] = useState([]);
-
-  const getCombos = async () => {
-    const response = await axios.get("combos?with=services"); // Eager Loading
-    setCombos(response.data.data);
-  };
-
-  const onChange = (event) => {
-    const { name, value } = event.target;
-  
-    // Si el campo es "service_id", verificamos si es un arreglo o no
-    const updatedValue = name === 'service_id' ? Array.from(value) : value;
-  
-    setFormValues((prevValues) => ({
-      ...prevValues,
-      [name]: updatedValue,
-    }));
-  };
-  
-
-  const getCombo = async (id) => {
-    const response = await axios.get("combos/" + id);
-    const apiCombo = response.data.data;
-  
-    // Obtener todos los service_id del combo actual
-    const selectedServiceIds = apiCombo.services.map(service => service.service_id);
-console.log(selectedServiceIds);
-  
-    setCombo(apiCombo);
-    setFormValues({
-      name: apiCombo.name,
-      service_id: selectedServiceIds, // Actualizar el nombre del campo
-      price: apiCombo.price,
-      discount: apiCombo.discount,
-      total_price: apiCombo.total_price,
-    });
-  };
-  
-
-  useEffect(() => {
-    const fetchServices = async () => {
-      try {
-        const response = await axios.get('/services');
-        setServices(response.data.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchServices();
-  }, []);
-
-  const storeCombo = async (e) => {
-    e.preventDefault();
-    try {
-      await axios.post("combos", {
-        name: formValues.name,
-        price: formValues.price,
-        discount: formValues.discount,
-        total_price: formValues.total_price,
-        service_id: formValues.service_id // Cambio en el nombre del campo
-      });
-      setFormValues(initialForm);
-      navigate("/combos");
-    } catch (e) {
-      if (e.response.status === 422) {
-        setErrors(e.response.data.errors);
-      }
-    }
-  };
-
-  const updateCombo = async (e) => {
-    e.preventDefault();
-    try {
-      await axios.put("combos/" + combo.id, formValues);
-      setFormValues(initialForm);
-      navigate("/combos");
-    } catch (e) {
-      setErrors(e.response.data.errors);
-      if (e.response.status === 422) {
-        // Lógica adicional para manejar errores de validación si es necesario
-      }
-    }
-  };
-
-  const deleteCombo = async (id) => {
-    if (!window.confirm("Estás seguro?")) {
-      return;
-    }
-    await axios.delete("combos/" + id);
-    getCombos();
-  };
-
-  return (
-    <ComboContext.Provider value={{ combo, combos, getCombo, getCombos, onChange, formValues, storeCombo, errors, updateCombo, deleteCombo, setErrors, services, setServices }}>{children}</ComboContext.Provider>
-  );
+const initialForm = {
+name: "",
+service_id: [],
+price: "",
+discount: "",
+total_price: ""
 };
+
+//VARIABLES INICIALIZADAS
+const [formValues, setFormValues] = useState(initialForm);
+const [combos, setCombos] = useState([]);
+const [combo, setCombo] = useState([]);
+const [errors, setErrors] = useState({});
+const navigate = useNavigate();
+const [services, setServices] = useState([]);
+const [selectedServices, setSelectedServices] = useState([]);
+
+const [totalPrice, setTotalPrice] = useState(0);
+const [discount, setDiscount] = useState(0);
+const [discountedPrice, setDiscountedPrice] = useState(0);
+
+//SELECT MATERIAL UI PARA CREATE Y EDIT
+const MenuProps = {
+PaperProps: {
+style: {
+maxHeight: 224,
+width: 250,
+},
+},
+};
+
+//FUNCION QUE USA EL SELECT MATERIAL UI
+const handleChange = (event) => {
+  const selectedServices = event.target.value || [];
+  setSelectedServices(selectedServices);
+};
+
+//SERVICIOS
+useEffect(() => {
+  const fetchData = async () => {
+  try {
+  const response = await axios.get('/services');
+  setServices(response.data.data);
+  
+  setFormValues(prevFormValues => ({
+  ...prevFormValues,
+  service_id: selectedServices
+  }));
+  } catch (error) {
+  console.log(error);
+  }
+  };
+  
+  fetchData();
+  }, [selectedServices]);
+
+//FUNCIÓN QUE TIENE QUE MANEJAR TODOS LOS CAMBIOS DEL FORMULARIO
+const onChange = (e) => {
+const { name, value} = e.target;
+setFormValues({...formValues, [name]: value});
+}
+
+//FUNCIÓN QUE TRAE TODOS LOS COMBOS AL INDEX
+const getCombos = async () => {
+const apiCombos = await axios.get("combos");
+setCombos(apiCombos.data.data);
+};
+//FUNCIÓN QUE TRAE UN ID ESPECÍFICO PARA EDITAR
+const getCombo = async (id) => {
+  const response = await axios.get("combos/" + id);
+  const apiCombo = response.data.data;
+  setSelectedServices(apiCombo.services);
+  setFormValues({
+    ...apiCombo,
+    service_id: apiCombo.services.map((service) => service.id).filter(Boolean)
+  });
+};
+
+
+//FUNCIÓN QUE GUARDA UN NUEVO COMBO
+const storeCombo = async (e) => {
+e.preventDefault();
+const combo = {
+name: formValues.name,
+services: selectedServices.map((service) => service.id),
+price: formValues.priceTotal,
+discount: formValues.discount,
+total_price: formValues.discountedPrice
+};
+try{
+await axios.post("combos", formValues);
+setFormValues(initialForm);
+navigate("/combos");
+} catch(e){
+if(e.response.status === 422){
+setErrors(e.response.data.errors);
+}
+}
+setFormValues(initialForm);
+setSelectedServices([]);
+setDiscount(0);
+setDiscountedPrice(0);
+setTotalPrice(0);
+setErrors({});
+}
+//FUNCIÓN QUE ACTUALIZA UN COMBO
+const updateCombo = async (e) => {
+e.preventDefault();
+const updatedCombo = {
+...formValues,
+services: selectedServices.map((service) => service.id),
+price: totalPrice,
+discount: formValues.discount,
+total_price: discountedPrice,
+};
+try {
+await axios.put("combos/" + combo.id, updatedCombo);
+setFormValues(initialForm);
+navigate("/combos");
+} catch (e) {
+setErrors(e.response.data.errors);
+if (e.response.status === 422) {
+// Manejar el error de validación si es necesario
+}
+}
+};
+
+//FUNCIÓN PARA BORRAR UN COMBO
+const deleteCombo = async (id) => {
+if(!window.confirm("Estás seguro?")){
+return;
+}
+await axios.delete("combos/" + id);
+getCombos();
+}
+
+
+const calculateDiscountedPrice = () => {
+if (selectedServices.length > 0) {
+const selectedPrices = selectedServices.map(service => parseFloat(service.price));
+const totalPrice = selectedPrices.reduce((acc, curr) => acc + curr);
+setTotalPrice(totalPrice);
+const discount = parseFloat(formValues["discount"]) || 0;
+const discountedPrice = totalPrice * (1 - discount / 100);
+setDiscountedPrice(discountedPrice);
+} else {
+setTotalPrice(0);
+setDiscountedPrice(0);
+}
+};
+useEffect(calculateDiscountedPrice, [selectedServices, formValues]);
+
+
+
+//EXPORTANDO LAS FUNCIONES
+
+return <ComboContext.Provider value={{
+combo,
+combos, 
+getCombo, 
+getCombos, 
+onChange, 
+formValues, 
+storeCombo, 
+errors, 
+updateCombo, 
+deleteCombo, 
+setErrors, 
+services, 
+setServices, 
+setCombos,
+setCombo,
+selectedServices,
+setSelectedServices,
+handleChange,
+MenuProps,
+discount,
+setDiscount,
+discountedPrice,
+setDiscountedPrice,
+totalPrice,
+setTotalPrice, 
+setFormValues
+}}>{children}
+</ComboContext.Provider>
+}
 
 export default ComboContext;
--- src/components/combos/ComboEdit.jsx
+++ src/components/combos/ComboEdit.jsx
@@ -1,5 +1,8 @@
-import React, { useContext, useEffect } from "react";
+import { useContext, useEffect } from "react";
 import ComboContext from "../../Context/ComboContext";
+import * as React from 'react';
+import { useParams } from "react-router-dom"; 
+
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
@@ -7,90 +10,95 @@ import FormControl from '@mui/material/FormControl';
 import ListItemText from '@mui/material/ListItemText';
 import Select from '@mui/material/Select';
 import Checkbox from '@mui/material/Checkbox';
-import { useParams } from "react-router-dom";
 
 export const ComboEdit = () => {
-  const { formValues, onChange, errors, setErrors, combo, getCombo, updateCombo, services } = useContext(ComboContext);
-const selectedServiceIds = formValues.service_id || []; // Inicializar como un arreglo vacío si es undefined
+const {
+formValues,
+onChange,
+errors,
+setErrors,
+combo,
+getCombo,
+updateCombo,
+services,
+selectedServices,
+handleChange,
+MenuProps,
+discountedPrice,
+totalPrice,
+setFormValues
+} = useContext(ComboContext);
 
-  let { id } = useParams();
-  useEffect(() => {
-    getCombo(id);
-    setErrors({});
-  }, []);
+let { id } = useParams();
+useEffect(() => {
+getCombo(id);
+setErrors({});
+}, [])
+useEffect(() => {
+setFormValues((prevFormValues) => ({
+...prevFormValues,
+price: totalPrice,
+total_price: discountedPrice
+}));
+}, [totalPrice, discountedPrice, setFormValues]);
+return (
+<div className="mt-12">
+<form onSubmit={updateCombo} className="max-w-md mx-auto p-4 bg-white shadow-md rounded-sm">
+<div className="space-y-6">
+<div className="mb-4">
+<label htmlFor="nombre" className="block mb-2 text-sm font-medium">Nombre</label>
+<input name="name" value={formValues["name"]} onChange={onChange} className="border border-gray-300 text-gray-900 text-sm rounded-md block w-full p-2" />
+{errors.name && <span className="text-sm text-red-400">{ errors.name[0]}</span>}
+</div>
 
-  const ITEM_HEIGHT = 48;
-  const ITEM_PADDING_TOP = 8;
-  const MenuProps = {
-    PaperProps: {
-      style: {
-        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-        width: 250,
-      },
-    },
-  };
-
-  return (
-    <div className="mt-12">
-      <form onSubmit={updateCombo} className="max-w-md mx-auto p-4 bg-white shadow-md rounded-sm">
-        <div className="space-y-6">
-          <div className="mb-4">
-            <label htmlFor="nombre" className="block mb-2 text-sm font-medium">Nombre</label>
-            <input name="name" value={formValues["name"]} onChange={onChange} className="border border-gray-300 text-gray-900 text-sm rounded-md block w-full p-2" />
-            {errors.name && <span className="text-sm text-red-400">{errors.name[0]}</span>}
-          </div>
-
-          <div className="mb-4">
-            <label className="block mb-2 text-sm font-medium">Servicio</label>
-            <Select
-  labelId="demo-multiple-checkbox-label"
-  id="demo-multiple-checkbox"
-  name="service_id"
-  multiple
-  value={selectedServiceIds}
-  onChange={onChange}
-  input={<OutlinedInput label="Servicios" />}
-  renderValue={(selected) =>
-    selected
-      .map((value) => {
-        const service = services.find((service) => service.id === value);
-        return service ? service.name : "";
-      })
-      .join(", ")
-  }
-  MenuProps={MenuProps}
+<div className="mb-4">
+<FormControl sx={{ m: 1, width: 300 }}>
+<InputLabel id="demo-multiple-checkbox-label">Servicios</InputLabel>
+<Select
+labelId="demo-multiple-checkbox-label"
+id="demo-multiple-checkbox"
+multiple
+value={selectedServices} // Actualizado
+onChange={handleChange}
+input={<OutlinedInput label="Servicios" />}
+renderValue={(selected) =>
+selected.map((service) => service && service.name).filter(Boolean).join(", ")
+}
+MenuProps={MenuProps}
 >
-  {services.map((service) => (
-    <MenuItem key={service.id} value={service.id}>
-      <Checkbox checked={(selectedServiceIds || []).includes(service.id)} />
-      <ListItemText primary={service.name} />
-    </MenuItem>
-  ))}
+<MenuItem value="">
+<em>Selecciona un servicio</em>
+</MenuItem>
+{services.map((service) => (
+<MenuItem key={service.id} value={service}>
+<Checkbox checked={selectedServices.some((s) => s.id === service.id)} />
+<ListItemText primary={service.name} secondary={`$${service.price}`} />
+</MenuItem>
+))}
 </Select>
-
-
-            {errors.service_id && <span className="text-sm text-red-400">{errors.service_id[0]}</span>}
-          </div>
-          <div className="mb-4">
-            <label htmlFor="precio" className="block mb-2 text-sm font-medium">Precio</label>
-            <input name="price" value={formValues["price"]} onChange={onChange} className="border border-gray-300 text-gray-900 text-sm rounded-md block w-full p-2" />
-            {errors.price && <span className="text-sm text-red-400">{errors.price[0]}</span>}
-          </div>
-          <div className="mb-4">
-            <label htmlFor="descuento" className="block mb-2 text-sm font-medium">Descuento</label>
-            <input name="discount" value={formValues["discount"]} onChange={onChange} className="border border-gray-300 text-gray-900 text-sm rounded-md block w-full p-2" />
-            {errors.discount && <span className="text-sm text-red-400">{errors.discount[0]}</span>}
-          </div>
-          <div className="mb-4">
-            <label htmlFor="total" className="block mb-2 text-sm font-medium">Total</label>
-            <input name="total_price" value={formValues["total_price"]} onChange={onChange} className="border border-gray-300 text-gray-900 text-sm rounded-md block w-full p-2" />
-            {errors.total_price && <span className="text-sm text-red-400">{errors.total_price[0]}</span>}
-          </div>
-        </div>
-        <div className="my-4">
-          <button className="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded-md">Actualizar</button>
-        </div>
-      </form>
-    </div>
-  );
-};
+{errors.service_id && <span className="text-sm text-red-400">{ errors.service_id[0]}</span>}
+</FormControl>
+</div>
+<div className="mb-4">
+<label htmlFor="precio" className="block mb-2 text-sm font-medium">Precio</label>
+<input name="price" value={totalPrice} onChange={onChange} className="border border-gray-300 text-gray-900 text-sm rounded-md block w-full p-2" />
+{errors.price && <span className="text-sm text-red-400">{ errors.price[0]}</span>}
+</div>
+<div className="mb-4">
+<label htmlFor="descuento" className="block mb-2 text-sm font-medium">Descuento</label>
+<input name="discount" value={formValues["discount"]} onChange={onChange} className="border border-gray-300 text-gray-900 text-sm rounded-md block w-full p-2" />
+{errors.discount && <span className="text-sm text-red-400">{ errors.discount[0]}</span>}
+</div>
+<div className="mb-4">
+<label htmlFor="total" className="block mb-2 text-sm font-medium">Total</label>
+<input name="total_price" value={discountedPrice} onChange={onChange} className="border border-gray-300 text-gray-900 text-sm rounded-md block w-full p-2" />
+{errors.total_price && <span className="text-sm text-red-400">{ errors.total_price[0]}</span>}
+</div>
+</div>
+<div className="my-4">
+<button className="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 text-white rounded-md">Actualizar</button>
+</div>
+</form>
+</div>
+)
+}
